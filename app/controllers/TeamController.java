42563005b37da6c783d5b402cf02d0491ac815c9
Arnar Kari <arnarkari93@gmail.com>
2015-10-23 22:09:06 +0000

    added team controller

---
+++ app/controllers/TeamController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by arnarkari on 23/10/15.
+ *
+ * @author arnarkari
+ */
+public class TeamController extends Controller {
+    
+}
