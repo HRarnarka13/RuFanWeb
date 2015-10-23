dede2cc4b9a517114ad4921a8738db4b75fdcc01
Arnar Kari <arnarkari93@gmail.com>
2015-10-23 15:33:57 +0000

    added empty class for PlayerData

---
+++ app/is/ru/honn/rufanweb/data/PlayerData.java
@@ -0,0 +1,11 @@
+package is.ru.honn.rufanweb.data;
+
+import is.ruframework.data.RuData;
+
+/**
+ * Created by arnarkari on 23/10/15.
+ *
+ * @author arnarkari
+ */
+public class PlayerData extends RuData  {
+}
