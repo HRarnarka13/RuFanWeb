b5858c9325795c08672facd5e6d39bcb244953fc
Arnar Kari <arnarkari93@gmail.com>
2015-10-23 23:40:47 +0000

    adde dropdownlist for teams in signup view

--- app/controllers/SignupController.java
+++ app/controllers/SignupController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import is.rufan.team.domain.Team;
+import is.rufan.team.service.TeamService;
 import is.rufan.user.domain.UserRegistration;
 import is.rufan.user.service.UserService;
 import play.mvc.*;
@@ -8,58 +10,57 @@ import play.data.*;
 import views.html.signup;
 import views.html.summary;
 
+
+import java.util.List;
+
 import static play.data.Form.*;
 
 
 public class SignupController extends UserController
 {
-  final static Form<UserRegistration> signupForm = form(UserRegistration.class);
+    final static Form<UserRegistration> signupForm = form(UserRegistration.class);
 
-  public Result blank()
-  {
-    return ok(signup.render(signupForm));
-  }
-
-  public Result signup()
-  {
-    Form<UserRegistration> filledForm = signupForm.bindFromRequest();
-
-    UserService service = (UserService) ctx.getBean("userService");
-
-    if (!"true".equals(filledForm.field("accept").value()))
+    public Result blank()
     {
-      filledForm.reject("accept", "You must accept the terms and conditions");
+        TeamService teamService = (TeamService) ctx.getBean("teamService");
+        return ok(signup.render(signupForm, teamService.getTeams()));
     }
 
-    if(service.getUserByUsername(filledForm.field("username").value()) != null)
-    {
-        filledForm.reject("username", "Username already exists");
-    }
+    public Result signup() {
+        Form<UserRegistration> filledForm = signupForm.bindFromRequest();
 
-    if (filledForm.field("username").value().length() < 4)
-    {
-      filledForm.reject("username", "Display Name must be at least 4 characters");
-    }
 
-    if (!filledForm.field("password").value().equals(filledForm.field("repeatPassword").value()))
-    {
-      filledForm.reject("repeatPassword", "The passwords you entered don't match");
-    }
+        UserService service = (UserService) ctx.getBean("userService");
+        TeamService teamService = (TeamService) ctx.getBean("teamService");
 
-    if (filledForm.field("password").value().length() < 6)
-    {
-      filledForm.reject("password", "The password is too short");
-    }
+        if (!"true".equals(filledForm.field("accept").value())) {
+            filledForm.reject("accept", "You must accept the terms and conditions");
+        }
 
-    if (filledForm.hasErrors())
-    {
-      return badRequest(signup.render(filledForm));
+        if(service.getUserByUsername(filledForm.field("username").value()) != null) {
+            filledForm.reject("username", "Username already exists");
+        }
+
+        if (filledForm.field("username").value().length() < 4) {
+            filledForm.reject("username", "Display Name must be at least 4 characters");
+        }
+
+        if (!filledForm.field("password").value().equals(filledForm.field("repeatPassword").value())) {
+            filledForm.reject("repeatPassword", "The passwords you entered don't match");
+        }
+
+        if (filledForm.field("password").value().length() < 6) {
+            filledForm.reject("password", "The password is too short");
+        }
+
+        // System.out.println(filledForm.field("fav_team").value());
+
+        if (filledForm.hasErrors()) {
+            return badRequest(signup.render(filledForm, teamService.getTeams()));
+        } else {
+            UserRegistration created = filledForm.get();
+            service.addUser(created);
+            return ok(summary.render(created));
+        }
     }
-    else
-    {
-      UserRegistration created = filledForm.get();
-      service.addUser(created);
-      return ok(summary.render(created));
-    }
-  }
 }
--- app/controllers/TeamController.java
+++ app/controllers/TeamController.java
@@ -1,6 +1,13 @@
 package controllers;
 
+import is.rufan.team.data.TeamDataGateway;
+import is.rufan.team.domain.Team;
+import is.rufan.team.service.TeamService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import play.mvc.Controller;
+
+import java.util.List;
 
 /**
  * Created by arnarkari on 23/10/15.
@@ -8,5 +15,11 @@ import play.mvc.Controller;
  * @author arnarkari
  */
 public class TeamController extends Controller {
-    
+
+    protected ApplicationContext ctx = new FileSystemXmlApplicationContext("conf/userapp.xml");
+    TeamService service = (TeamService) ctx.getBean("teamService");
+
+    public List<Team> getTeams() {
+        return service.getTeams();
+    }
 }
